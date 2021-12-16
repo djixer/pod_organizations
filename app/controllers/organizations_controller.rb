dde51cc020909dcc752c76296b751111b24a2746
djixer <asv@djixer.com>
2021-12-16 22:13:08 +0300

    added table layout and links to get data

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,7 +3,6 @@ class OrganizationsController < ApplicationController
   before_action :set_organizations
 
   def index
-
   end
 
   def show
@@ -12,21 +11,17 @@ class OrganizationsController < ApplicationController
   private
 
   def set_organizations
-    auth_token = open('/usr/src/token').read
+    @auth_token = open('/usr/src/token').read
     require 'json'
     require 'net/http'
 
-    url = 'https://data.gov.ru/api/json/organization/?access_token=' + auth_token
+    url = 'https://data.gov.ru/api/json/organization/?access_token=' + @auth_token
     uri = URI(url)
     response = Net::HTTP.get(uri)
     @organizations = JSON.parse(response)
   end
 
   def set_organization
-    #@organization = @organizations[]
-  end
-
-  def set_token
 
   end
 
